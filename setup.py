f3393e56ad573df75690db5186bcd473bc3bdf7a
Okumu Joseph <jokumu63@yahoo.com>
2024-03-26 22:18:16 +0300

    Update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Healthcare services Chatbot',
+    version= '0.0.0',
+    author= 'Okumu Joseph',
+    author_email= 'jokumu63@yahoo.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
